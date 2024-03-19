bb52f3e299c719d58fff855db3c728f5e7f79589
Eliezer <ezgaming3377@gmail.com>
2024-03-19 17:49:47 -0400

    Complete? I think it's working lol

--- client.cpp
+++ client.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <netdb.h>
 #include <string>
+#include <fstream>
 
 
 using namespace std;
@@ -71,26 +72,107 @@ int main(int argc, char* argv[])
 	*/
 
 	HTTP_Request *request = HTTP_Request::Create_GET_request(uri->Get_path());
+	request->Set_host(uri->Get_host());
+
+	// HTTP Request string
 	std::string request_str;
 	request->Print(request_str);
 	std::cout << "Request string: " << request_str;
 
-	send(tcp_sock, &request, sizeof(request), 0);
+	send(tcp_sock, request_str.c_str(), request_str.length(), 0);
+	std::cout << "Sent request" << std::endl;
+
+
+	// Setup receiving
+	const unsigned int BUFFER_SIZE = 8192;
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+
+    int received_total = 0, received = 0;
+    bool headers_parsed = false;
+
+	// Output location
+	HTTP_Response* response;
+	ofstream file;
+	string filename = "index.html"; //default filename
 
 	// recv and then parse that info to check if its a valid response
+    while ((received = recv(tcp_sock, buffer + received_total, BUFFER_SIZE - received_total - 1, 0)) > 0) 
+	{
+        received_total += received;
+		std::cout << received_total << std::endl;
+
+        if (!headers_parsed) 
+		{
+            std::string headers(buffer, received_total);
+            size_t pos = headers.find("\r\n\r\n"); // Find the 2 carriages that signify the end of the headers
+            if (pos != string::npos) 
+			{
+                headers_parsed = true;
+                response = HTTP_Response::Parse(buffer, pos + 4); // Start processing the info
+
+				// Failed to parse the response from server
+                if (response == nullptr) 
+				{
+                    cerr << "Failed to parse HTTP response headers." << endl;
+                    return 6;
+                }
+
+                cout << "HTTP Status Code: " << response->Get_code() << endl;
+
+				// Status code received. We only care about status code 200. Anything else is treated as an error
+                if (response->Get_code() != 200) 
+				{
+                    cerr << "Request failed with status " << response->Get_code() << endl;
+                    return 7;
+                }
+
+                // Determine filename from URI
+                string path = uri->Get_path();
+                size_t last_slash_pos = path.find_last_of('/');
+
+				// filename = the last string after the last '/'
+                if (last_slash_pos != string::npos && last_slash_pos + 1 < path.length()) 
+				{
+                    filename = path.substr(last_slash_pos + 1);
+                }
+
+				// Open file for writing
+                file.open(filename, ios::binary);
+
+				// Error check for failing to open the file
+                if (!file.is_open()) 
+				{
+                    cerr << "Cannot open file: " << filename << endl;
+                    return 8;
+                }
+
+                // Write the initial part of the body to the file
+                file.write(buffer + pos + 4, received_total - pos - 4);
+            }
+        } 
+
+		// Continue writing tot he file
+		else if (file.is_open()) 
+		{
+            file.write(buffer, received);
+        }
 
 
-	// std::string response;
-	// if (!request->Get_header_value("HTTP/1.1", response))
-	// {
-	// 	std::cout << "Failed to read response from server"
-	// }
-	
+        memset(buffer, 0, BUFFER_SIZE); // Clear the buffer for the next read
+    }
 
+    if (received < 0) 
+	{
+        cerr << "Failed to receive data from server." << endl;
+        return 9;
+    }
 
+	// Close file and socket
+    file.close();
+    close(tcp_sock);
 
-
-	
-	close(tcp_sock);
-	return 0;
+	// SUCCESS
+    cout << "File " << filename << " has been saved." << endl;
+    return 0;
 }
